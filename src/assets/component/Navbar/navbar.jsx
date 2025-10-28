f14d27ce20c84050016779776d8c199a0650d41f
hananawad004 <hananawad004@gmail.com>
2025-10-28 03:22:55 +0200

    finish nav

---
+++ src/assets/component/Navbar/navbar.jsx
@@ -0,0 +1,48 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar mainNav navbar-expand-lg navbar-dark fixed-top">
+      <div className="container">
+        <a className="navbar-brand" href="#page-top">
+          START BOOTSTRAP
+        </a>
+
+        <button
+          className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          Menu
+          <i className="fas fa-bars ms-2"></i>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item mx-0 mx-lg-1">
+              <a className="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item mx-0 mx-lg-1">
+              <a className="nav-link py-3 px-0 px-lg-3 rounded" href="#about">
+                About
+              </a>
+            </li>
+            <li className="nav-item mx-0 mx-lg-1">
+              <a className="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
